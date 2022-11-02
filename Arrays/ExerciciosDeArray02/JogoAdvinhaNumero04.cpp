12f5c46f0f6df74af6e57929d5deecd577af877b
PedroFilho <psfilho81@hotmail.com>
2022-11-02 17:10:44 -0300

    Update JogoAdvinhaNumero04.cpp

--- Arrays/ExerciciosDeArray02/JogoAdvinhaNumero04.cpp
+++ Arrays/ExerciciosDeArray02/JogoAdvinhaNumero04.cpp
@@ -18,7 +18,7 @@
 using namespace std;
 
 // protótipos
-int adicionarNumero(vector<int> &); // adiciona número ao vetor
+int adicionarNumero(); // adiciona número ao vetor
 void exibirValores(vector<int> &); // exibe os valores do vetor
 
 int main()
@@ -26,8 +26,58 @@ int main()
     // muda a configuração para português brasileiro
     setlocale(LC_ALL, "Portuguese");
 
+    int sorteio = 0;
+    int palpite = 0;
+    char sentinela = 's';
+
     // vetor
     vector<int> numerosJogados = {0};
+
+    while(sentinela != 'n' || sentinela != 'N')
+    {
+        // gerador
+        srand( time( 0 ) );
+
+        // sorteio recebe valor aleatório
+        sorteio = adicionarNumero();
+        // vetor recebe o valor sorteado
+        numerosJogados.push_back(sorteio);
+
+        // informa a área do número sorteaeo
+        cout << "Estou pensando em um número entre 1 e 1000:"
+                << "\nVocê consegur adivinhá-lo? " << endl;
+
+        while( true )
+        {
+            cout << "Que número estou pensando? ";
+            cin >> palpite;
+
+            // se o valor do sorteio igual ao palpite
+            if( sorteio == palpite)
+            {
+                // imprima
+                cout << "Você venceu!" << endl;
+                break; // e saia do jogo
+            } // final if
+            else if(sorteio > palpite)
+            {
+                cout << "Digite um número maior." << endl;
+            }
+            else if(sorteio < palpite)
+            {
+                cout << "Digite um número menor." << endl;
+            }
+
+        } // final while interno
+
+        cout << "Deseja continuar jogando( s ou n )? ";
+        cin >> sentinela;
+        if(sentinela == 's' || sentinela == 'S')
+            system("cls");
+        else
+            break;
+
+    } // final while externo
 
     cout << endl; // nova linha
 
@@ -38,11 +88,8 @@ int main()
 } // final principal
 
 // adicionarNumero
-int adicionarNumero(vector<int> &vetor)
+int adicionarNumero()
 {
-    srand( time( 0 ) );
-
-    vetor[]
-    return 1 + rand() % 100;
+    return 1 + rand() % 1000;
 
 } // final adicionarNumero
