685003d5e63bbe4f95d6dd1cd67a6e0852c06a72
PedroFilho <psfilho81@hotmail.com>
2022-11-17 17:23:58 -0300

    Update PonteirosComArray09.cpp

--- Ponteiros/Exeplos/PonteirosComArray09.cpp
+++ Ponteiros/Exeplos/PonteirosComArray09.cpp
@@ -21,14 +21,18 @@ int main()
     const int arraySize = 10; // tamanho do array
     int valor[arraySize] = {5, 2, 6, 8, 10, 1, 25, 83, 3, 4};
 
+    //mostra o vetor original
     cout << "Array original: " << endl;
     for( int i = 0; i < arraySize; i++)
         cout << setw(4) << valor[ i ];
 
+    // chama a função para organizar o vetor
     selectionSort(valor, arraySize); // ordena o array
 
+    // cabeçalho
     cout << "\nArray em ordem crescente: " << endl;
 
+    // loop para ostrar o vetor organizado
     for(int j = 0; j < arraySize; j++)
         cout << setw(4) << valor[ j ];
 
@@ -46,10 +50,15 @@ void selectionSort(int * const vetor, const int sizeVetor)
     int menorVl;
     for(int i = 0; i < sizeVetor - 1; i++)
     {
+        // variável recebo o valor de i
         menorVl = i; // primeiro índice do array
 
+        // loop para organizar o vetor
         for( int index = i + 1; index < sizeVetor; index++)
+            // verifica se o vetor do o index é menor
+            // que o vetor com o menor valor
             if(vetor[index] < vetor[menorVl])
+                // se verdade, menor valor recebe o index
                 menorVl = index;
 
         swapVetor( &vetor[ i ], &vetor[ menorVl ] );
