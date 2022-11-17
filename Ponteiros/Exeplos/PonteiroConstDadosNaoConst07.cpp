e9e53e2add435f5094800cec9263216ed28e0e02
PedroFilho <psfilho81@hotmail.com>
2022-11-17 12:09:19 -0300

    Create PonteiroConstDadosNaoConst07.cpp

---
+++ Ponteiros/Exeplos/PonteiroConstDadosNaoConst07.cpp
@@ -0,0 +1,37 @@
+/*
+    tentar modificar um ponteiro const para dados não constante
+*/
+#include <iostream>
+#include <clocale>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    // muda a configuração para português brasileiro
+    setlocale(LC_ALL, "Portuguese");
+
+    int x, y;
+
+    /*
+        ptr é um ponteiro constante para um inteiro que    pode
+        ser modificado por ptr, mas ptr sepre aponta para a mesma
+        posição da meória;
+    */
+    // constante deve ser inicializada
+    int * const ptr = &x; // ponteiro constante recebe endereço de x
+
+    // ptr recebe valor
+    *ptr = 7; // ptr não é const
+
+    ptr = &y; // Erro: ptr é constante, não é possível atribuí-lo a um novo endereço.
+
+    cout << endl; // nova linha
+
+    system("pause"); // pausa do programa
+
+    return 0;
+
+} // final principal
